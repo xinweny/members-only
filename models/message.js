625769ebdd7b4fc7ea3a42d30329c1dcee3ab81a
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-03-31 11:03:25 -0400

    rename text field to body

--- models/message.js
+++ models/message.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
   title: { type: String, required: true, minLength: 1 },
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true, minLength: 1 },
+  body: { type: String, required: true, minLength: 1 },
   createdAt: { type: Date, required: true },
 });
 

93a33803fc18bc6425714ea522cd21bd17f1582e
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-03-31 17:26:55 -0400

    add date formatter

--- models/message.js
+++ models/message.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const MessageSchema = new Schema({
   title: { type: String, required: true, minLength: 1 },
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   body: { type: String, required: true, minLength: 1 },
   createdAt: { type: Date, required: true },
+});
+
+MessageSchema.virtual('date_formatted').get(function () {
+  return DateTime.fromJSDate(this.createdAt).toLocaleString(DateTime.DATETIME_MED);
 });
 
 module.exports = mongoose.model('Message', MessageSchema);

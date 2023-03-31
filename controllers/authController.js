96191f12c0d17749aea7a7cde2a987482271f2ea
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-03-31 12:10:40 -0400

    add signup and login/logout functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,31 +1,46 @@
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 const User = require('../models/user');
 
-exports.getSignupForm = (req, res, next) => {
+exports.getSignupForm = (req, res) => { res.render('signup_form'); };
+
+exports.getLoginForm = (req, res) => { res.render('login_form'); };
+
+exports.signup = async (req, res, next) => {
   try {
-    res.render('signup');
+    bcrypt.hash(
+      req.body.password,
+      10,
+      async (err, hashedPwd) => {
+        if (err) return next(err);
+
+        const user = new User({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          password: hashedPwd,
+          membership_status: 'Non-member',
+        });
+
+        await user.save();
+
+        res.redirect('/');
+      }
+    );
   } catch (err) {
     return next(err);
   }
 };
 
-exports.getLoginForm = (req, res, next) => {
-  try {
-    res.render('login');
-  } catch (err) {
-    return next(err);
-  }
-};
-
-exports.signup = (req, res, next) => {
-
-};
-
-exports.login = (req, res, next) => {
-
-};
+exports.login = passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+});
 
 exports.logout = (req, res, next) => {
-
+  req.logout(err => {
+    if (err) return next(err);
+    res.redirect('/');
+  })
 };
